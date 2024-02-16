dc756c5694d0ff80a816e097aacecba54c3e6c20
abdeccan <abdeccan@gmail.com>
2024-02-16 17:33:19 +0530

    renaming project

---
+++ NETTestApp/TeamVoting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETTestApp
+{
+    internal class TeamVoting
+    {   public string RankTeams(string[] votes)
+        {
+            if (votes.Length == 1)
+                return votes[0];
+
+            return String.Empty;
+        }
+    }
+}
